10c45d8c152bf821d9e9030c5dfc60edb7d2051c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-08 23:07:33 -0500

    add option for users to change their email

--- db/migrate/20220709034732_add_c_onfirmable_to_users.rb
+++ db/migrate/20220709034732_add_c_onfirmable_to_users.rb
@@ -3,6 +3,7 @@ class AddCOnfirmableToUsers < ActiveRecord::Migration[7.0]
     add_column :users, :confirmation_token, :string
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
+    add_column :users, :unconfirmed_email, :string 
 
     add_index :users, :confirmation_token, unique: true
 
@@ -11,6 +12,6 @@ class AddCOnfirmableToUsers < ActiveRecord::Migration[7.0]
 
   def down
     remove_index :users, :confirmation_token
-    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at
+    remove_columns :users, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email
   end
 end
