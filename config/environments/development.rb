33ade0d5cf9843af329cf5fc01e43bf0d920c839
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-12 11:04:15 -0500

    add MailCatcher configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,12 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  # ! added from MailCatcher
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+  config.action_mailer.raise_delivery_errors = false
+  # ! localhost of frontend 
+  config.action_mailer.default_url_options = { host: 'localhost:3001' }
 
   config.action_mailer.perform_caching = false
 
