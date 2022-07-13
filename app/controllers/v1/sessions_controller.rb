7e5720ca8147b58acf66e98fe52f05712d230bae
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-13 11:14:34 -0500

    render json for wrong credentials

--- app/controllers/v1/sessions_controller.rb
+++ app/controllers/v1/sessions_controller.rb
@@ -4,12 +4,10 @@ class V1::SessionsController < ApplicationController
         
         #! user&. same as user && user.valid_password?
         if @user&.valid_password?(params[:user][:password])
-            @current_user = @user
             jwt = JWT.encode ({user_id: @user.id, exp: (Time.now + 2.weeks).to_i}), Rails.application.secrets.secret_key_base, 'HS256'
-            puts @current_user.email
             render :create, locals: {token: jwt}, status: :created
         else
-            head(:unauthorized)
+            render json: { error: 'invalid_credentials' }, status: :unauthorized
         end
            
     end
