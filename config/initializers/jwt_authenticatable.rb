ecc2f0a9791dacffebe62a99288e4875477d67e0
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-13 15:18:45 -0500

    use WebToken from .lib dir for decoding JWT token

--- config/initializers/jwt_authenticatable.rb
+++ config/initializers/jwt_authenticatable.rb
@@ -5,7 +5,8 @@ module Devise
                 token = get_token
                 return fail(:invalid) unless token
 
-                payload = get_payload 
+                # payload = get_payload 
+                payload = WebToken.decode(token)
                 return fail(:invalid) if payload == :expired
 
                 resource = mapping.to.find(payload['user_id'])
@@ -16,15 +17,15 @@ module Devise
 
             private 
 
-            def get_payload
-                JWT.decode(
-                    get_token, 
-                    Rails.application.secrets.secret_key_base, 
-                    'HS256'
-                    ).first
-            rescue JWT::ExpiredSignature
-                :expired
-            end
+            # def get_payload
+            #     JWT.decode(
+            #         get_token, 
+            #         Rails.application.secrets.secret_key_base, 
+            #         'HS256'
+            #         ).first
+            # rescue JWT::ExpiredSignature
+            #     :expired
+            # end
             
             def get_token
                 auth_header.present? && auth_header.split(" ").last
