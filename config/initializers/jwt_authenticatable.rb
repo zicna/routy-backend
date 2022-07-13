9db2314ca125acbb80a10672f9d9637f23a599d1
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-13 13:02:36 -0500

    integrate devise part I

---
+++ config/initializers/jwt_authenticatable.rb
@@ -0,0 +1,38 @@
+module Devise
+    module Strategies
+        class JWTAuthenticatable < Base
+            def authenticate!
+                token = get_token
+                return fail(:invalid) unless token
+
+                paylaod = get_payload 
+                return fail(:invalid) if payload == :expired
+
+                resource = mapping.to.find(payload['user_id'])
+                return fail(:not_found_in_database) unless resource
+
+                success! resource
+            end
+
+            private 
+
+            def get_payload
+                JWT.decode(
+                    get_token, 
+                    Rails.application.secrets.secret_key_base, 
+                    'HS256'
+                    ).first
+            rescue JWT::ExpiredSignature
+                :expired
+            end
+            
+            def get_token
+                auth_header.present? && auth_header.split(" ").last
+            end
+
+            def auth_header
+                request.headers["Authorization"]
+            end
+        end
+    end
+end
