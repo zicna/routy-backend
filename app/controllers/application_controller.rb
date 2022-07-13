f7249300af938d19bc7ab492ff0c42ade11cc51f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-13 11:06:39 -0500

    refactor cirrent_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,24 @@
 class ApplicationController < ActionController::API
+    
     def current_user
-        # byebug
-        jwt = request.headers["Authorization"].split(" ").last
+        @current_user ||=  User.where(id: decode_token).first
+    end
 
+    private def get_token
+        request.headers["Authorization"].split(" ").last
+    end 
 
-        user_id = JWT.decode(jwt, Rails.application.secrets.secret_key_base, 'HS256').first['user_id']
-
-        @current_user =  User.where(id: user_id)
-
+    private def decode_token
+        JWT.decode(get_token, Rails.application.secrets.secret_key_base, 'HS256').first['user_id']
     end
     
-    # ! when using devise controllers
+    
+    # ! permit parameters and add non default for devise (when using devise controllers)
 #     before_action :configure_permitted_parameters, if: :devise_controller?
 
 #   protected
 
 #   def configure_permitted_parameters
-#     # byebug
 #     # devise_parameter_sanitizer.for(:user).push(:username)
 #     devise_parameter_sanitizer.permit(:user, keys: [:username])
 #   end
