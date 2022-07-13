d2308f9f7098212e5a302329afca15b5143e7a38
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-13 10:34:27 -0500

    add current_user method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::API
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    def current_user
+        # byebug
+        jwt = request.headers["Authorization"].split(" ").last
 
-  protected
 
-  def configure_permitted_parameters
-    # byebug
-    # devise_parameter_sanitizer.for(:user).push(:username)
-    devise_parameter_sanitizer.permit(:user, keys: [:username])
-  end
+        user_id = JWT.decode(jwt, Rails.application.secrets.secret_key_base, 'HS256').first['user_id']
+
+        @current_user =  User.where(id: user_id)
+
+    end
+    
+    # ! when using devise controllers
+#     before_action :configure_permitted_parameters, if: :devise_controller?
+
+#   protected
+
+#   def configure_permitted_parameters
+#     # byebug
+#     # devise_parameter_sanitizer.for(:user).push(:username)
+#     devise_parameter_sanitizer.permit(:user, keys: [:username])
+#   end
 end
