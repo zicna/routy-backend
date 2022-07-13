bae825e58d8b372e051ff4f05783bbf44863fca0
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-13 15:12:31 -0500

    configure routes to use devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+    defaults: { format: :json },
+    path: '',
+    path_names: {
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
+    },
+    controllers: {
+      registrations: "v1/users",
+      sessions: 'v1/sessions'
+    }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   namespace :v1, path: "", defaults: {format: :json} do
-    resources :users, only: [:create, :show]
-    resources :sessions, only: [:create, :destroy]
+    resources :users, only: [ :show]
   end
+  # resources :sessions, only: [:create, :destroy]
 end
 
 
