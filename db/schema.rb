e742d8dcbe666b610348aaca8b3577421a354d31
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-07-15 11:37:43 -0500

    add accociations for models, migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_06_212755) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_162958) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pins", force: :cascade do |t|
+    t.bigint "route_id", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["route_id"], name: "index_pins_on_route_id"
+  end
+
+  create_table "routes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_routes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -32,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_06_212755) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "pins", "routes"
+  add_foreign_key "routes", "users"
 end
